8112789429f18a9f30676d42e105b5cf53d0d60b
Viacheslav Petrenko <v-vpetrenko@hotels.com>
2020-08-20 16:18:55 +0300

    tests

--- packer/elastic_test.py
+++ packer/elastic_test.py
@@ -1,4 +1,10 @@
-def test_elasticsearch_is_installed(host):
-    elasticsearch = host.package("elasticsearch")
-    assert elasticsearch.is_installed
-    assert elasticsearch.version.startswith("7.9")
+import pytest
+
+@pytest.mark.parametrize("name,version", [
+    ("elasticsearch", "7.9"),
+    ("openjdk-11-jre", "11"),
+])
+def test_packages(host, name, version):
+    pkg = host.package(name)
+    assert pkg.is_installed
+    assert pkg.version.startswith(version)
